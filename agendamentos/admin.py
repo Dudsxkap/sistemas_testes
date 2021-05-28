bb6ad4417cf25162dac6f8afb9e70dad5c5cb8e7
Eduardo Amorim <eduardo.araujo@academico.ifs.edu.br>
2021-05-28 03:59:02 -0300

    Criação dos registros no Admin

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from agendamentos.models import Agendamentos, SalasAgendamento, AgendamentosDisponiveis, Sala, LocalVacinacao, Vacina, \
+    GruposAtendimento#, Cidadao
+# Talvez colocar cidadao depois
+admin.site.register(Agendamentos)
+admin.site.register(SalasAgendamento)
+admin.site.register(AgendamentosDisponiveis)
+admin.site.register(Sala)
+admin.site.register(LocalVacinacao)
+admin.site.register(Vacina)
+admin.site.register(GruposAtendimento)
 
-# Register your models here.
