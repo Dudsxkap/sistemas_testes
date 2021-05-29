53e72793ec0a30677ed95bb5489d65cb6867a287
Eduardo Amorim <eduardo.araujo@academico.ifs.edu.br>
2021-05-29 02:58:59 -0300

    Criação das classes ModelAdmin

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,12 +1,50 @@
 from django.contrib import admin
 from agendamentos.models import Agendamentos, SalasAgendamento, AgendamentosDisponiveis, Sala, LocalVacinacao, Vacina, \
-    GruposAtendimento#, Cidadao
-# Talvez colocar cidadao depois
-admin.site.register(Agendamentos)
-admin.site.register(SalasAgendamento)
-admin.site.register(AgendamentosDisponiveis)
-admin.site.register(Sala)
-admin.site.register(LocalVacinacao)
-admin.site.register(Vacina)
-admin.site.register(GruposAtendimento)
+    GruposAtendimento
 
+admin.site.site_header = 'Administração do site VacinAÇÃO'
+admin.site.site_title = 'Administração do site VacinAÇÃO'
+
+
+class VacinaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'fabricante')
+    list_filter = ('nome', 'fabricante')
+
+
+class GruposAtendimentoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'idade_minima')
+    list_filter = ('nome', 'idade_minima')
+
+
+class LocalVacinacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'logradouro', 'bairro', 'cidade')
+    list_filter = ('logradouro', 'bairro', 'cidade')
+
+
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nome_local')
+    list_filter = ('nome',)
+
+
+class SalasAgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome_sala', 'data_agendamento', 'horario_agendamento', 'numero_vagas')
+
+
+class AgendamentosAdmin(admin.ModelAdmin):
+    list_display = ('nome_cidadao', 'nome_grupo', 'data_agendamento', 'horario_agendamento', 'status')
+    list_filter = ('status',)
+
+
+class AgendamentosDisponiveisAdmin(admin.ModelAdmin):
+    list_display = ('nome_vacina', 'data', 'horario')
+    list_filter = ('data', 'horario')
+
+
+# Talvez colocar cidadao depois
+admin.site.register(Agendamentos, AgendamentosAdmin)
+admin.site.register(SalasAgendamento, SalasAgendamentoAdmin)
+admin.site.register(AgendamentosDisponiveis, AgendamentosDisponiveisAdmin)
+admin.site.register(Sala, SalaAdmin)
+admin.site.register(LocalVacinacao, LocalVacinacaoAdmin)
+admin.site.register(Vacina, VacinaAdmin)
+admin.site.register(GruposAtendimento, GruposAtendimentoAdmin)
