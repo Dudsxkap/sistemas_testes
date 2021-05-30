e4771c648f27ad2a5ac9cc8e22eb6e64a508ce08
Eduardo Amorim <eduardo.araujo@academico.ifs.edu.br>
2021-05-30 03:31:01 -0300

    Criação de Usuário pelo Admin

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 from agendamentos.models import Agendamentos, SalasAgendamento, AgendamentosDisponiveis, Sala, LocalVacinacao, Vacina, \
     GruposAtendimento
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from agendamentos.forms import RegisterForm, UserAdminChangeForm
+
+Usuario = get_user_model()
+
+admin.site.unregister(Group)
 
 admin.site.site_header = 'Administração do site VacinAÇÃO'
 admin.site.site_title = 'Administração do site VacinAÇÃO'
+
+
+class UsuarioAdmin(BaseUserAdmin):
+    add_form = RegisterForm
+    form = UserAdminChangeForm
+
+    list_display = ('nome', 'email', 'data_nascimento', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('nome', 'email', 'data_nascimento', 'password')}),
+        ('Permissões', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('nome', 'data_nascimento', 'email', 'senha', 'senha_confirmada', 'is_admin')}
+         ),
+    )
+    search_fields = ['nome', 'email']
+    ordering = ['nome', 'email']
+    filter_horizontal = ()
 
 
 class VacinaAdmin(admin.ModelAdmin):
@@ -40,7 +70,7 @@ class AgendamentosDisponiveisAdmin(admin.ModelAdmin):
     list_filter = ('data', 'horario')
 
 
-# Talvez colocar cidadao depois
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Agendamentos, AgendamentosAdmin)
 admin.site.register(SalasAgendamento, SalasAgendamentoAdmin)
 admin.site.register(AgendamentosDisponiveis, AgendamentosDisponiveisAdmin)
