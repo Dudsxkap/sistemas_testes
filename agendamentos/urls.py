b7415ff149b5a874e315bfa79a9be797eca4e63c
Eduardo Amorim <eduardo.araujo@academico.ifs.edu.br>
2021-06-02 04:26:59 -0300

    Adição das urls para as páginas do app

--- agendamentos/urls.py
+++ agendamentos/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from agendamentos.forms import UserLoginForm
 from agendamentos import views
+from .views import IndexView, DadosJSONView
 
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('cadastro/', views.cadastro_usuario_view, name='cadastro_usuario'),
+    path('logout/', views.deslogar, name='logout'),
+    path('teste/', views.teste, name='teste'),
+    path('agendamento/', views.agendamento, name='agendamento'),
+    path('agendamentos_disponiveis/', views.agendamentos_disponiveis, name='agendamentos_disponiveis'),
+    path('meus_agendamentos/', views.meus_agendamentos, name='meus_agendamentos'),
 
+]
+urlpatterns += [
+    path('', IndexView.as_view(), name='index'),
+    path('dados/', DadosJSONView.as_view(), name='dados'),
 ]
