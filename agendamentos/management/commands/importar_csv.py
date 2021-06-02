ce0a2b29dae3b28ed3de8d1bbe3de163fc6b3984
Eduardo Amorim <eduardo.araujo@academico.ifs.edu.br>
2021-06-02 18:57:06 -0300

    Atualização no command de importação de csv

--- agendamentos/management/commands/importar_csv.py
+++ agendamentos/management/commands/importar_csv.py
@@ -11,13 +11,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            locais_vacinacao = []
             print("Iniciando importação do CSV, por favor aguarde...")
             ubs = pd.read_csv("agendamentos/csv_importacao/ubs.csv")
             ubs.apply(
-                lambda x: LocalVacinacao.objects.update_or_create(
-                    defaults={"nome": x["nom_estab"], "logradouro": x["dsc_endereco"], "bairro": x["dsc_bairro"],
-                              "cidade": x["dsc_cidade"]}, cnes=x["cod_cnes"]),
-                axis=1)
+                lambda x: locais_vacinacao.append(
+                    LocalVacinacao(nome=x["nom_estab"], logradouro=x["dsc_endereco"], bairro=x["dsc_bairro"],
+                                   cidade=x["dsc_cidade"], cnes=x["cod_cnes"]))
+                , axis=1)
+            LocalVacinacao.objects.bulk_create(locais_vacinacao)
             print("Importação concluída!")
         except FileNotFoundError:
             print("Arquivo CSV não encontrado, certifique-se de colocar o arquivo CSV no diretório correto.")
