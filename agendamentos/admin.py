8b5d0c99d6e11eec493ee5f0362b77324b47db77
Eduardo Amorim <atenas503@gmail.com>
2024-01-06 18:39:27 -0300

    feat: Add models no admin

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,61 +1,22 @@
 from django.contrib import admin
-from agendamentos.models import Agendamento, AgendamentoDisponivel, EstabelecimentoSaude, GrupoAtendimento
-from django.contrib.auth import get_user_model
+from agendamentos.models import User, Agendamento, AgendamentoDisponivel, EstabelecimentoSaude, GrupoAtendimento, \
+    Cidadao
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-Usuario = get_user_model()
 
 admin.site.unregister(Group)
 
-admin.site.site_header = 'Administração do site VacinAÇÃO'
-admin.site.site_title = 'Administração do site VacinAÇÃO'
-
-
-"""class UsuarioAdmin(BaseUserAdmin):
-    add_form = RegisterForm
-    form = UserAdminChangeForm
-
-    list_display = ('nome', 'email', 'data_nascimento', 'is_admin')
-    list_filter = ('is_admin',)
-    fieldsets = (
-        (None, {'fields': ('nome', 'email', 'data_nascimento', 'password')}),
-        ('Permissões', {'fields': ('is_admin',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('nome', 'data_nascimento', 'email', 'senha', 'senha_confirmada', 'is_admin')}
-         ),
-    )
-    search_fields = ['nome', 'email']
-    ordering = ['nome', 'email']
-    filter_horizontal = ()
-
-
-class GrupoAtendimentoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'idade_minima')
-    list_filter = ('nome', 'idade_minima')
+admin.site.site_header = 'Administração do site'
+admin.site.site_title = 'Administração do site'
 
 
 class EstabelecimentoSaudeAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'logradouro', 'bairro', 'cidade')
-    list_filter = ('logradouro', 'bairro', 'cidade')
+    list_display = ('nome', 'cnes',)
+    list_filter = ('nome', 'cnes',)
 
 
-class AgendamentoAdmin(admin.ModelAdmin):
-    list_display = ('nome_cidadao', 'nome_grupo', 'data_agendamento', 'horario_agendamento', 'status')
-    list_filter = ('status',)
-
-
-class AgendamentoDisponivelAdmin(admin.ModelAdmin):
-    list_display = ('nome_local', 'nome_vacina', 'data', 'horario', 'num_vagas')
-    list_filter = ('data', 'horario')
-
-
-admin.site.register(Usuario, UsuarioAdmin)
-admin.site.register(Agendamento, AgendamentoAdmin)
-admin.site.register(AgendamentoDisponivel, AgendamentoDisponivelAdmin)
+admin.site.register(User)
+admin.site.register(Cidadao)
+admin.site.register(Agendamento)
+admin.site.register(AgendamentoDisponivel)
 admin.site.register(EstabelecimentoSaude, EstabelecimentoSaudeAdmin)
-admin.site.register(GrupoAtendimento, GrupoAtendimentoAdmin)"""
+admin.site.register(GrupoAtendimento)
