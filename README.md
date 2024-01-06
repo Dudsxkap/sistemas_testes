ba62cdf992f1cf3f7c89247589c8903e342b41a9
Eduardo Amorim <atenas503@gmail.com>
2024-01-06 14:48:25 -0300

    build: Ajustes config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VacinAÇÃO
+# Agendamento de Testes
 
 ## Descrição do Projeto
 Sistema desenvolvido para agendamento de testes do participante Eduardo Amorim dos Santos Araujo para o processo seletivo EDITAL Nº 028/2023 – LAIS/UFRN, Fase 2.
--- sistema_testes/asgi.py
+++ sistema_testes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sistema_vacinas project.
+ASGI config for sistema_testes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_vacinas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_testes.settings')
 
 application = get_asgi_application()
